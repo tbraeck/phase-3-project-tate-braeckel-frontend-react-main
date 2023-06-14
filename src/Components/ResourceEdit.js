95c9829d132b80eb2e936b388d9085d228f2db5c
Tate Braeckel <tatebraeckel@gmail.com>
2023-06-14 10:49:55 -0500

    cmmit wednesday

--- src/Components/ResourceEdit.js
+++ src/Components/ResourceEdit.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ResourceEdit 
+// import ResourceEdit 
 
 const ResourceEdit = ({ resource, onUpdated }) => {
   const [name, setName] = useState('');
