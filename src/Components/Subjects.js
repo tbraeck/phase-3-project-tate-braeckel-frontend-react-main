4c902666341334ebde370f95e972744708726b53
Tate Braeckel <tatebraeckel@gmail.com>
2023-06-21 17:06:06 -0500

    new commit

--- src/Components/Subjects.js
+++ src/Components/Subjects.js
@@ -1,41 +1,38 @@
 import React from 'react'
-import Search from './Search'
-import { useState} from 'react'
+// import Search from './Search'
+// import { useState} from 'react'
 import { Link } from 'react-router-dom'
 
 
 
 function Subjects({subjects}) {
-    const [searchTerm, setSearchTerm] = useState("");
     
-    const filteredSubjects = subjects.filter((subject) => {
-      return subject.name.toLowerCase().includes(searchTerm.toLowerCase());
-    });
+    //
 
 return (
-  <div>
-  <div className='subjectList'>
- <div className='subjectsTitle'>
- <h1 >SUBJECTS</h1>
- </div>
- <div className='subjectMap  ' >
+        <div>
+        <div className='subjectList'>
+      <div className='subjectsTitle'>
+      <h1 >SUBJECTS</h1>
+      </div>
+      <div className='subjectMap  ' >
 
- {filteredSubjects.map((subject) => (
+      {subjects.map((subject) => (
 
-  <div key={subject.id}>
-  <h2  >
-<Link to={`/subjects/${subject.id}`}  >
-  {subject.name}
-</Link>
-  </h2>
-  </div>
- ))}
- </div>
-  </div>
+        <div key={subject.id}>
+        <h2 >
+      <Link to={`/subjects/${subject.id}`}  >
+        {subject.name}
+      </Link>
+        </h2>
+        </div>
+      ))}
+      </div>
+        </div>
 
-<div className='searchBar'>
+{/* <div className='searchBar'>
   <Search className='searchBarReal' searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-  </div>
+  </div> */}
     </div>
   )
 }
