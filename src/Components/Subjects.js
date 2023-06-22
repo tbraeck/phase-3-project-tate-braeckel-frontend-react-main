ff288234e9bf8f3a584bc3d2e38f1a3252c594bb
Tate Braeckel <tatebraeckel@gmail.com>
2023-06-22 11:59:50 -0500

    thursday

--- src/Components/Subjects.js
+++ src/Components/Subjects.js
@@ -1,13 +1,18 @@
 import React from 'react'
 // import Search from './Search'
-// import { useState} from 'react'
 import { Link } from 'react-router-dom'
 
-
-
 function Subjects({subjects}) {
-    
-    //
+
+  const links = subjects.map((subject) => (
+    <div key={subject.id}>
+    <h2 >
+  <Link to={`/subjects/${subject.id}`}  >
+    {subject.name}
+  </Link>
+    </h2>
+    </div>
+  ))
 
 return (
         <div>
@@ -16,17 +21,7 @@ return (
       <h1 >SUBJECTS</h1>
       </div>
       <div className='subjectMap  ' >
-
-      {subjects.map((subject) => (
-
-        <div key={subject.id}>
-        <h2 >
-      <Link to={`/subjects/${subject.id}`}  >
-        {subject.name}
-      </Link>
-        </h2>
-        </div>
-      ))}
+      {links}
       </div>
         </div>
 
